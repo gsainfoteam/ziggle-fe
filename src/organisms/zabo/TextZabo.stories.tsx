ee7f73968b6d75715ca5c3f80b68b977b1db4e0e
2paperstar <lee@paperst.ar>
2023-05-05 09:37:56 +0900

    feat: add text zabo

---
+++ src/organisms/zabo/TextZabo.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryFn } from "@storybook/react";
+
+import TextZabo from "./TextZabo";
+
+export default {
+  title: "organisms/zabo/TextZabo",
+  component: TextZabo,
+} as Meta<typeof TextZabo>;
+
+const Template: StoryFn<typeof TextZabo> = (args) => <TextZabo {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: "맥북 프로 주인을 찾습니다.",
+  content:
+    "2월 11일쯤 해동에서 맥북 프로를 분실하신 분을 찾습니다. 24시간 넘게 안 가져가시길래 제가 일단 보관하기로 했고 이렇게 게시글 올립니다. 글 본 분들은 주변에 알려주시고 혹시나 주인분이 보신다면 01044445555로 연락 부탁드립니다.",
+  date: "2023.02.13",
+  viewCount: 119,
+  author: "박시원",
+};
