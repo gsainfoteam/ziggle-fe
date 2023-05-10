6bac11807cca5ef6508d603ce478010c10c55d1e
Crown3 <strikedown7@gmail.com>
2023-05-10 15:11:02 +0900

    added secondary, colorless color and hover animation of button atoms

--- src/styles/colorSet.ts
+++ src/styles/colorSet.ts
@@ -1,8 +1,12 @@
 const colorSet = {
   primary: "#FF6565",
+  secondary: "#FFEAEA", // primary보다 연함
   placeholder: "#E3E3E3",
+
   text: "#252525", // CSS 기본 "black"과의 혼동을 피하기 위해 Figma의 "black"을 "text"로 표기
   secondaryText: "#959595",
+
+  colorless: "#FFFFFF", // CSS 기본 "white"과의 혼동을 피하기 위해 "colorless"로 표기
 };
 
 export default colorSet;
