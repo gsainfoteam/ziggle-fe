5ad40e535fce4f82717f0f9502513e92308d1200
Crown3 <strikedown7@gmail.com>
2023-05-12 19:54:12 +0900

    added horizontal scroll button to ZaboCarousel, and added button animations

--- src/molecules/horizontalScrollButton/HorizontalScrollButton.tsx
+++ src/molecules/horizontalScrollButton/HorizontalScrollButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import Button from "../../atoms/button/Button";
 import Flex from "../../atoms/containers/flex/Flex";
@@ -7,6 +8,13 @@ import Icon from "../../atoms/icon/Icon";
 interface HorizontalScrollButtonProps {
   children?: React.ReactNode;
 }
+
+const AnimatedBtn = styled(Button)`
+  transition: transform 0.1s ease-in-out;
+  :active {
+    transform: scale(0.9);
+  }
+`;
 
 const HorizontalScrollButton = ({ children }: HorizontalScrollButtonProps) => {
   return <Flex gap={"12px"}>{children}</Flex>;
@@ -19,25 +27,25 @@ interface ButtonProps {
 
 const Left = ({ disabled = false, onClick }: ButtonProps) => {
   return (
-    <Button onClick={onClick}>
+    <AnimatedBtn onClick={onClick}>
       {disabled ? (
         <Icon.ArrowCircleLeftDeselected width={"40px"} height={"40px"} />
       ) : (
         <Icon.ArrowCircleLeftPrimary width={"40px"} height={"40px"} />
       )}
-    </Button>
+    </AnimatedBtn>
   );
 };
 
 const Right = ({ disabled = false, onClick }: ButtonProps) => {
   return (
-    <Button onClick={onClick}>
+    <AnimatedBtn onClick={onClick}>
       {disabled ? (
         <Icon.ArrowCircleRightDeselected width={"40px"} height={"40px"} />
       ) : (
         <Icon.ArrowCircleRightPrimary width={"40px"} height={"40px"} />
       )}
-    </Button>
+    </AnimatedBtn>
   );
 };
 
