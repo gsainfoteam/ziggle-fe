a6bb6d62e70eeb1571bdc78ac9a49293628eeca4
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-06-23 01:18:20 +0900

    [FEATURE] mypage article list

--- src/pages/myPage/myPage.tsx
+++ src/pages/myPage/myPage.tsx
@@ -15,7 +15,7 @@ const myPage = () => {
             <Content>
 
             <div style={{ display: 'flex' }}>
-                    <div style={{ flex: 2, display: 'flex', flexDirection: 'column'}}>
+                    <div style={{ flex: 2, display: 'flex', flexDirection: 'column',position: 'relative', margin:'10px'}}>
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
                         <Text size="40px" color={colorSet.text} font={Font.Medium} style={{paddingTop:'20px',justifyContent: 'center', alignItems: 'center', letterSpacing: '10px'}}>
                             INFO
@@ -50,18 +50,29 @@ const myPage = () => {
                             infoteam@gm.gist.ac.kr
                         </Text>
                     </div>
+                    <div style={{ position: 'absolute', top: 0, bottom: 0, right: 0, width: '1px', background: '#ccc', boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)' }}></div>
                     </div>
 
-                    <div style={{ flex: 3, backgroundColor: '#e0e0e0' }}>
+                    <div style={{ flex: 3,  display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 
 
                 <div>
-                    <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+                    <table style={{ borderCollapse: 'collapse', width: '500px', margin:'10px', borderRadius:'10px' }}>
                   <tbody>
-                    <tr>
-                      <td style={{ borderBottom: '1px solid #ccc' }}>
-                        20210000</td>
+                  <tr style={{ height: '50px', backgroundColor: colorSet.primary, borderRadius: '10px 0 0 10px'}}>
+                  <td style={{ borderBottom: '1px solid #ccc', width:'75%' }}>
+                        내가 게시한 공지 목록</td>
+                        <td style={{borderBottom: '1px solid #ccc', width: '25%' }}>
+                        </td>
                     </tr>
+                    <tr>
+                      <td style={{ borderBottom: '1px solid #ccc', width:'75%' }}>
+                        20210000</td>
+                        <td style={{borderBottom: '1px solid #ccc', width: '25%' }}>
+                        날짜
+                        </td>
+                    </tr>
+                    
                     <tr>
                       <td style={{ borderBottom: '1px solid #ccc' }}>
                         010-1234-1234</td>
