2df4b56d6fc21d150320be92198dc62d4ec4fffe
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-06-25 01:40:02 +0900

    [FEATURE] mypage article table 1

--- src/pages/myPage/myPage.tsx
+++ src/pages/myPage/myPage.tsx
@@ -57,48 +57,72 @@ const myPage = () => {
                 
 
                 <div>
-                <table style={{ borderCollapse: 'collapse', width: '500px', margin:'10px', borderRadius:'10px' }}>
-                  <tbody>
-                  <tr style={{ height: '50px', backgroundColor: colorSet.primary, borderRadius: '10px 10px 0 0'}}>
-                  <td style={{ borderBottom: '1px solid #ccc', width:'75%', borderRadius: '10px 0 0 0' }}>
-                        내가 게시한 공지 목록</td>
-                        <td style={{borderBottom: '1px solid #ccc', width: '25%', borderRadius: '0 10px 0 0 ' }}>
-                        </td>
+
+                <table style={{ width: '500px', margin: '10px', borderCollapse: 'separate', borderSpacing: '0', borderRadius: '10px' }}>
+                <tbody>
+                    <tr style={{ height: '50px', backgroundColor: colorSet.primary }}>
+                    <td style={{ width: '75%', borderTopLeftRadius: '10px' }}>
+                        <Text size={'17px'} color={colorSet.colorless} font={Font.Medium} style={{paddingLeft:'20px'}} >
+                        내가 게시한 공지 목록
+                        </Text>
+                    </td>
+                    <td style={{ width: '25%', borderTopRightRadius: '15px'}}>
+                    </td>
                     </tr>
-                    <tr style={{ height: '50px'}}>
-                      <td style={{ borderBottom: '1px solid #ccc', width:'75%' }}>
-                        23학번 과잠 구매 희망자 조사</td>
-                        <td style={{borderBottom: '1px solid #ccc', width: '25%' }}>
+                    <tr style={{ height: '50px' }}>
+                    <td style={{ borderLeft: '1px solid #ccc', borderBottom: '1px solid #ccc', width: '75%' }}>
+                        <Text size={'15px'} color={colorSet.text} font={Font.Regular} style={{paddingLeft:'20px'}}>
+                        23학번 과잠 구매 희망자 조사
+                        </Text>
+                    </td>
+                    <td style={{ borderRight: '1px solid #ccc', borderBottom: '1px solid #ccc', width: '25%' }}>
+                    <Text size={'15px'} color={colorSet.text} font={Font.Regular} style={{padding:'10px', textAlign:'center'}}>
                         2023.02.14.
-                        </td>
-                    </tr>
-                    
-                    <tr style={{ height: '50px'}}>
-                      <td style={{ borderBottom: '1px solid #ccc' }}>
-                        공연동아리 지대로 연극 ART </td>
-                    <td style={{borderBottom: '1px solid #ccc', width: '25%' }}>
-                    2023.02.13.
+                        </Text>
                     </td>
                     </tr>
-                    <tr style={{ height: '50px'}}>
-                      <td style={{ borderBottom: '1px solid #ccc' }}>
-                        2023년도 WING 신규 부원 모집!</td>
-                    <td style={{borderBottom: '1px solid #ccc', width: '25%' }}>
-                    2023.02.13.
+                    <tr style={{ height: '50px' }}>
+                    <td style={{ borderLeft: '1px solid #ccc', borderBottom: '1px solid #ccc' }}>
+                    <Text size={'15px'} color={colorSet.text} font={Font.Regular} style={{paddingLeft:'20px'}}>
+                        공연동아리 지대로 연극 'ART'
+                        </Text>
+                    </td>
+                    <td style={{ borderRight: '1px solid #ccc', borderBottom: '1px solid #ccc', width: '25%' }}>
+                    <Text size={'15px'} color={colorSet.text} font={Font.Regular} style={{padding:'10px', textAlign:'center'}}>
+                        2023.02.13.
+                        </Text>
                     </td>
                     </tr>
-                    <tr style={{ height: '50px', borderRadius: '10px 10px 0 0'}} >
-                      <td style={{ borderLeft: '1px solid #ccc',  borderRadius: '10px 10px 0 0', borderBottom: '1px solid #ccc' }}>
-                        23년도 인포팀 신규 부원 모집</td>
-                    <td style={{borderRight: '1px solid #ccc',  borderRadius: '10px 10px 0 0', borderBottom: '1px solid #ccc', width: '25%' }}>
-                    2023.02.13.
+                    <tr style={{ height: '50px' }}>
+                    <td style={{ borderLeft: '1px solid #ccc', borderBottom: '1px solid #ccc' }}>
+                    <Text size={'15px'} color={colorSet.text} font={Font.Regular} style={{paddingLeft:'20px'}}>
+                        2023년도 WING 신규 부원 모집!
+                        </Text>
+                    </td>
+                    <td style={{ borderRight: '1px solid #ccc', borderBottom: '1px solid #ccc', width: '25%' }}>
+                    <Text size={'15px'} color={colorSet.text} font={Font.Regular} style={{padding:'10px', textAlign:'center'}}>
+                        2023.02.13.
+                        </Text>
                     </td>
                     </tr>
-                  </tbody>
+                    <tr style={{ height: '50px' }} >
+                    <td style={{ borderBottomLeftRadius: '10px', borderLeft: '1px solid #ccc', borderBottom: '1px solid #ccc' }}>
+                    <Text size={'15px'} color={colorSet.text} font={Font.Regular} style={{paddingLeft:'20px'}}>
+                        23년도 인포팀 신규 부원 모집
+                        </Text>
+                    </td>
+                    <td style={{ borderBottomRightRadius: '10px', borderRight: '1px solid #ccc', borderBottom: '1px solid #ccc', width: '25%' }}>
+                    <Text size={'15px'} color={colorSet.text} font={Font.Regular} style={{padding:'10px', textAlign:'center'}}>
+                        2023.02.13.
+                        </Text>
+                    </td>
+                    </tr>
+                </tbody>
                 </table>
+
                 </div>
-                    </div>
-            </div>
+                </div>
+                </div>
 
 
             </Content>
