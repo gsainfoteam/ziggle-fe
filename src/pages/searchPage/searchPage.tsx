578b1789922ed92c0b5a5d0f40aa9ed055a18c36
Shimsuyeon <suyeonshim@gm.gist.ac.kr>
2023-06-28 21:11:29 +0900

    [FEATURE] 16-searchPage font size edit

--- src/pages/searchPage/searchPage.tsx
+++ src/pages/searchPage/searchPage.tsx
@@ -45,105 +45,110 @@ const SearchPage = () => {
     <>
     
     <Area>
-    <Navbar background-color={colorSet.primary}></Navbar>
-    <Content>
-    <Button style = {{background:colorSet.placeholder}} onClick= {result}>result</Button> 
-    <Button onClick= {noResult} style = {{margin:"10px"}}>noResult</Button>
-    <div style = {{height:"150px", margin : '0 auto',display : 'flex', justifyContent: 'flex-end'}}>
+        <Navbar background-color={colorSet.primary}/>
+            <Content>
+                <Button style = {{background:colorSet.placeholder}} onClick= {result}>result</Button> 
+                <Button onClick= {noResult} style = {{margin:"10px"}}>noResult</Button>
 
-    <Button>
-    <XXPrimary size="25px" color={colorSet.secondaryText}/>
-    </Button>
-    
-    </div>
-    <p style = {{ paddingLeft : defaults.pageSideGap, paddingRight : defaults.pageSideGap}}>
-            <p style={{ width: '700px', margin: '0 auto' }}>   
-                <SearchVar onSubmit={handleSubmit} placeholder={"공지 제목이나 태그로 검색"}/>
-                <p style = {{height : "30px", margin : '0 auto'}}></p>    
-                <p style={{ height: "40px", margin:'0 auto'}}>
-                    <SearchTagSelect selected={selectedTags} onChange={handleTagChange}/>
+                <div style = {{height:"150px", margin : '0 auto',display : 'flex', justifyContent: 'flex-end'}}>
+
+                    <Button>
+                        <XXPrimary size="25px" color={colorSet.secondaryText}/>
+                    </Button>
+                
+                </div>
+            <p style = {{ paddingLeft : defaults.pageSideGap, paddingRight : defaults.pageSideGap}}>
+                <p style={{ width: '700px', margin: '0 auto' }}>   
+                    <SearchVar onSubmit={handleSubmit} placeholder={"공지 제목이나 태그로 검색"}/>
+                    <p style = {{height : "30px", margin : '0 auto'}}/>   
+                    <p style={{ height: "40px", margin:'0 auto'}}>
+                        <SearchTagSelect selected={selectedTags} onChange={handleTagChange}/>
+                    </p>
+                <p style = {{ flexGrow:1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin:'50px' }}>
+
+                    {defaultResults && (
+                        <p>
+                        <Search size="250px" color={colorSet.secondaryText}  ></Search>
+                        <p style = {{height : "10px", margin : '0 auto'}}></p> 
+                        <Text size="1.7rem" color={colorSet.secondaryText} font={Font.Medium} style={{paddingTop:'20px'}}>
+                                        검색어를 입력해주세요
+                        </Text>
+                        </p>
+                    )} 
+
+
+                    {showResults && (
+                        <p>
+                            <p style = {{paddingTop:'0px', paddingBottom:'0px', paddingLeft:defaults.pageSideGap,paddingRight:defaults.pageSideGap, height:"100px", margin : '0 auto',display : 'flex', justifyContent: 'flex-start'}}>
+                                <Text size="3.5rem" color={colorSet.text} font={Font.Bold} style={{padding:0}}>
+                                ♨ 지글 공지</Text>
+                        </p>
+                            {Array.from({ length: n }).map((_, index) =>(
+                                <div style={{ margin: '20px' }}>
+                                    <SearchResult
+                                        key={index} // 고유한 key prop을 지정해야 함
+                                        deadline="2023.02.28"
+                                        title="인포팀 신규 부원 모집"
+                                        author="이정우"
+                                        tags={["인포팀", "신규부원", "모집"]}
+                                        date="2023.02.13"
+                                        viewCount={123}
+                                        thumbnailUrl="https://picsum.photos/2000/3000"
+                                        searchQuery="이"
+                                    />
+                                </div>
+                            ))}
+
+                        <p style = {{paddingTop:'10px', 
+                                    paddingBottom:'0px', 
+                                    paddingLeft:defaults.pageSideGap,
+                                    paddingRight:defaults.pageSideGap, 
+                                    height:"100px", 
+                                    margin : '0 auto',
+                                    display : 'flex', 
+                                    justifyContent: 'flex-start'}}>
+                                    
+                                <Text size="3.5rem" color={colorSet.text} font={Font.Bold} style={{padding:'0px'}}>
+                                    📰 학사 공지
+                                </Text>
+                        </p>
+                            {Array.from({ length: n }).map((_) =>(
+                                <div style={{ margin: '20px' }}>
+                                <SearchResultText
+                                deadline="2023.02.28"
+                                title="인포팀 신규 부원 모집"
+                                author="이정우"
+                                tags={["모집", "WING", "많관부"]}
+                                date="2023.02.13"
+                                viewCount={123}
+                                content="안녕하세요, 하우스연합회입니다. 2023년 3월 1일, 하우스연합회에서 중고장터를 진행합니다❗퇴사자, 잔류자, 신입생 상관없이 판매 물품/무료나눔하고자하는 물품이 있으시다면 자유롭게 등록해주시기 바랍니다! 자세한 일정은 다음..."
+                                searchQuery="이"
+                                thumbnailUrl=""
+                                />
+                                </div>
+                                ))}
+                        </p>
+                    )}
+
+
+                    {noResults && (
+
+                        <p>
+                            <p style = {{height : "10px", margin : '0 auto'}}></p> 
+                            <SearchNoResult></SearchNoResult>
+                            <Text size="1.5rem" color={colorSet.secondaryText} font={Font.Bold} style={{paddingTop:'20px'}}>
+                                검색 결과가 존재하지 않습니다.
+                            </Text>
+                        </p>
+  
+
+                    )}
                 </p>
-    <p style = {{ flexGrow:1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', margin:'50px' }}>
-
-        {defaultResults && (
-            <p>
-            <Search size="250px" color={colorSet.secondaryText}  ></Search>
-            <p style = {{height : "10px", margin : '0 auto'}}></p> 
-            <Text size="25px" color={colorSet.secondaryText} font={Font.Medium} style={{paddingTop:'20px'}}>
-                            검색어를 입력해주세요
-            </Text>
+                </p>
             </p>
-        )} 
-
-
-        {showResults && (
-
-            <p>
-            <p style = {{paddingTop:'0px', paddingBottom:'0px', paddingLeft:defaults.pageSideGap,paddingRight:defaults.pageSideGap, height:"100px", margin : '0 auto',display : 'flex', justifyContent: 'flex-start'}}>
-                <Text size="50px" color={colorSet.text} font={Font.Bold} style={{padding:0}}>
-                ♨ 지글 공지
-            </Text>
-            </p>
-            
-    {Array.from({ length: n }).map((_, index) =>(
-    <div style={{ margin: '20px' }}>
-      <SearchResult
-        key={index} // 고유한 key prop을 지정해야 함
-        deadline="2023.02.28"
-        title="인포팀 신규 부원 모집"
-        author="이정우"
-        tags={["인포팀", "신규부원", "모집"]}
-        date="2023.02.13"
-        viewCount={123}
-        thumbnailUrl="https://picsum.photos/2000/3000"
-        searchQuery="이"
-      />
-    </div>
-    ))}
-
-            <p style = {{paddingTop:'10px', paddingBottom:'0px', paddingLeft:defaults.pageSideGap,paddingRight:defaults.pageSideGap, height:"100px", margin : '0 auto',display : 'flex', justifyContent: 'flex-start'}}>
-                <Text size="50px" color={colorSet.text} font={Font.Bold} style={{padding:'0px'}}>
-                📰 학사 공지
-            </Text>
-            </p>
-        {Array.from({ length: n }).map((_) =>(
-            <div style={{ margin: '20px' }}>
-            <SearchResultText
-            deadline="2023.02.28"
-            title="인포팀 신규 부원 모집"
-            author="이정우"
-            tags={["모집", "WING", "많관부"]}
-            date="2023.02.13"
-            viewCount={123}
-            content="안녕하세요, 하우스연합회입니다. 2023년 3월 1일, 하우스연합회에서 중고장터를 진행합니다❗퇴사자, 잔류자, 신입생 상관없이 판매 물품/무료나눔하고자하는 물품이 있으시다면 자유롭게 등록해주시기 바랍니다! 자세한 일정은 다음..."
-            searchQuery="이"
-            thumbnailUrl=""
-            />
-            </div>
-            ))}
-        </p>
-          )}
-
-
-          {noResults && (
-        <p>
-            <p>
-            <p style = {{height : "10px", margin : '0 auto'}}></p> 
-            <SearchNoResult></SearchNoResult>
-            <Text size="25px" color={colorSet.secondaryText} font={Font.Bold} style={{paddingTop:'20px'}}>
-                검색 결과가 존재하지 않습니다.
-            </Text>
-            </p>
-
-        </p>    
-        
-        )}
-        </p>
-        </p>
-    </p>
-    <p style = {{height:"300px", margin : '0 auto'}}></p>
-    </Content>
-    <Footer></Footer>
+            <p style = {{height:"300px", margin : '0 auto'}}/>
+            </Content>
+        <Footer/>
     </Area>
     
 </>
