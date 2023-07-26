2cd212d20738782e9869cef132764c2a45372aeb
Shim su yeon <suyeonshim@gm.gist.ac.kr>
2023-07-26 20:43:40 +0900

    fluid grid  mypage fix

--- src/pages/myPage/myPage.tsx
+++ src/pages/myPage/myPage.tsx
@@ -1,5 +1,5 @@
+import { useMediaQuery } from "react-responsive";
 import Area from "src/atoms/containers/area/Area";
-import Content from "src/atoms/containers/content/Content";
 import dummyMypageArticles from "src/mock/dummy-mypage-articles";
 import MyPageInfo from "src/mock/dummy-mypage-info";
 import MypageProfile from "src/pages/myPage/MypageProfile";
@@ -7,65 +7,61 @@ import MypageSeperate from "src/pages/myPage/MypageSeperate";
 import MypageTable from "src/pages/myPage/MypageTable";
 
 const MyPage = () => {
+  const isMobile = useMediaQuery({ maxWidth: 1000 });
   return (
     <>
       <Area>
-        <Content
+        <div
           style={{
-            width: "1000px",
-            height: "1000px",
             display: "flex",
-            flexDirection: "column",
+            height: "1000px",
             alignItems: "center",
+            flexDirection: isMobile ? "column" : "row",
           }}
         >
           <div
-            style={{ display: "flex", height: "1000px", alignItems: "center" }}
+            style={{
+              flex: 2,
+              display: "flex",
+              flexDirection: "column",
+              position: "relative",
+              margin: "10px",
+            }}
           >
-            <div
-              style={{
-                flex: 2,
-                display: "flex",
-                flexDirection: "column",
-                position: "relative",
-                margin: "10px",
-              }}
-            >
-              <MypageProfile
-                name={MyPageInfo.name}
-                id={MyPageInfo.id}
-                phone={MyPageInfo.phone}
-                email={MyPageInfo.email}
-              />
+            <MypageProfile
+              name={MyPageInfo.name}
+              id={MyPageInfo.id}
+              phone={MyPageInfo.phone}
+              email={MyPageInfo.email}
+            />
 
-              <MypageSeperate />
+            {!isMobile && <MypageSeperate />}
+          </div>
+
+          <div
+            style={{
+              flex: 3,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <div style={{ padding: "50px" }}>
+              <MypageTable
+                title="내가 게시한 공지 목록"
+                articles={dummyMypageArticles.articles2}
+              />
             </div>
 
-            <div
-              style={{
-                flex: 3,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <div style={{ padding: "50px" }}>
-                <MypageTable
-                  title="내가 게시한 공지 목록"
-                  articles={dummyMypageArticles.articles2}
-                />
-              </div>
-
-              <div>
-                <MypageTable
-                  title="리마인드 설정한 게시물 목록"
-                  articles={dummyMypageArticles.articles3}
-                />
-              </div>
+            <div>
+              <MypageTable
+                title="리마인드 설정한 게시물 목록"
+                articles={dummyMypageArticles.articles3}
+              />
             </div>
           </div>
-        </Content>
+        </div>
       </Area>
     </>
   );
