a627423a608e9efb3e421a4768fc4b3aba46d2fc
Dohyun-Ko <dohyun682@gmail.com>
2023-08-08 16:20:37 +0900

    :recycle: Fix a line

--- src/apis/user/user-api.ts
+++ src/apis/user/user-api.ts
@@ -5,11 +5,14 @@ import { apiGetter } from "../interceptor/interceptor";
 
 export const goToIdp = () => {
   console.log(import.meta.env.NODE_ENV);
-  window.location.href = `${
+
+  const idp_url = `${
     import.meta.env.VITE_IDP_URL
-  }/authorize?client_id=ziggle2023&redirect_uri=${
+  }/oauth/authorize?client_id=ziggle2023&redirect_uri=${
     (import.meta.env.DEV ? "http://" : "https://") + window.location.host
   }&scope=openid%20profile%20email%20student_id&response_type=code`;
+
+  window.location.href = idp_url;
 };
 
 export const loginWithIdp = async ({
