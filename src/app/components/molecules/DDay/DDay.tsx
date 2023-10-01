ab79ac9b9ca2b4a43d9c4e09dcdddb76b6ac5a08
2paperstar <lee@paperst.ar>
2023-10-01 15:19:17 +0900

    feat: add dday molecule

---
+++ src/app/components/molecules/DDay/DDay.tsx
@@ -0,0 +1,30 @@
+import dayjs from 'dayjs';
+
+import { T } from '@/app/i18next';
+
+interface DDayProps {
+  deadline: dayjs.Dayjs;
+}
+
+const DDay = ({ deadline, t }: DDayProps & { t: T }) => (
+  <div
+    className={
+      'grid place-items-center w-fit ' +
+      'border border-white rounded bg-primary/90 px-2 py-1'
+    }
+  >
+    <div className="text-white text-sm md:text-lg font-bold">
+      {ddayFormatted(dayjs(deadline), t)}
+    </div>
+  </div>
+);
+
+const ddayFormatted = (deadline: dayjs.Dayjs, t: T) => {
+  const daysLeft = deadline.startOf('d').diff(dayjs().startOf('d'), 'd');
+
+  if (daysLeft < 0) return t('ddayPlus');
+  if (daysLeft === 0) return 'D - Day';
+  return `D - ${daysLeft}`;
+};
+
+export default DDay;
---
+++ src/app/components/molecules/DDay/index.ts
@@ -0,0 +1,1 @@
+export { default } from './DDay';
