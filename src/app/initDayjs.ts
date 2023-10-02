c846e8c3a543ba41003abaffacc7456b49dc6777
2paperstar <lee@paperst.ar>
2023-10-02 22:31:35 +0900

    fix: set default server timezone

--- src/app/initDayjs.ts
+++ src/app/initDayjs.ts
@@ -1,6 +1,11 @@
 import dayjs from 'dayjs';
 import ko from 'dayjs/locale/ko';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(LocalizedFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 dayjs.locale(ko);
+dayjs.tz.setDefault('Asia/Seoul');
