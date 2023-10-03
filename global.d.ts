22ee1acb4b885570dcc7b1c61fdc4c3a387d946f
2paperstar <lee@paperst.ar>
2023-10-03 11:28:59 +0900

    feat: graphql schema with webpack

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,9 @@ declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGSVGElement>>;
   export default content;
 }
+
+declare module '*.graphql' {
+  import { DocumentNode } from 'graphql';
+  const value: DocumentNode;
+  export default value;
+}
--- next.config.js
+++ next.config.js
@@ -3,21 +3,25 @@ const nextConfig = {
   images: {
     domains: ['s3.ap-northeast-2.amazonaws.com'],
   },
+  /** @argument {import('webpack').Configuration} config */
   webpack: (config) => {
     config.externals = [...config.externals, 'jsdom'];
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgoConfig: {
-              plugins: [{ name: 'removeViewBox', active: false }],
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgoConfig: {
+                plugins: [{ name: 'removeViewBox', active: false }],
+              },
             },
           },
-        },
-      ],
-    });
+        ],
+      },
+      { test: /\.graphql$/, use: [{ loader: 'graphql-tag/loader' }] },
+    );
     return config;
   },
 };
