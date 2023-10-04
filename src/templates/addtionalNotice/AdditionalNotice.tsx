4338fa74fab1fbcc8dad0c3eca3993fe722207f1
Shim su yeon <suyeonshim@gm.gist.ac.kr>
2023-10-04 13:13:39 +0900

    add deadline

--- src/templates/addtionalNotice/AdditionalNotice.tsx
+++ src/templates/addtionalNotice/AdditionalNotice.tsx
@@ -1,10 +1,39 @@
+import React, { useState } from "react";
+import LogEvents from "src/apis/log/log-event";
+import sendLog from "src/apis/log/sendLog";
 import Button, { ButtonVariant } from "src/atoms/button/Button";
+import Flex from "src/atoms/containers/flex/Flex";
 import Checkbox from "src/atoms/inputs/checkbox/Checkbox";
 import Text from "src/atoms/text/Text";
+import useIsMobile from "src/hooks/useIsMobile";
 import colorSet from "src/styles/colorSet";
 import Font from "src/styles/font";
+import { MOBILE_BREAKPOINT } from "src/types/types";
+import styled from "styled-components";
+
+import dateFormat from "../../utils/dateFormat";
+
+const DateInput = styled.input`
+  border: none;
+  font-size: 1.125rem;
+  font-family: "Noto Sans KR", sans-serif;
+  font-weight: 500;
+  color: ${colorSet.primary};
+  outline: none;
+
+  ::-webkit-calendar-picker-indicator {
+    // TODO: primary color로 바꾸기
+  }
+
+  @media (max-width: ${MOBILE_BREAKPOINT}) {
+    font-size: 0.875rem;
+  }
+`;
 
 const AdditionalNotice = () => {
+  const [hasDeadline, setHasDeadline] = useState<boolean>(false);
+  const [deadline, setDeadline] = useState<string>(dateFormat(new Date(), "-"));
+
   return (
     <Button width={"70%"} variant={ButtonVariant.outlined}>
       <div style={{ justifyContent: "left", display: "flex" }}>
@@ -12,6 +41,38 @@ const AdditionalNotice = () => {
           + 추가공지
         </Text>
       </div>
+      <Flex alignItems={"center"}>
+        <Checkbox
+          label={"마감일 설정"}
+          checked={hasDeadline}
+          onChange={(event) => {
+            setHasDeadline(event.target.checked);
+            sendLog(LogEvents.NoticeWritingPageCheckDeadline, {
+              checked: event.target.checked,
+            });
+          }}
+        />
+
+        {hasDeadline && deadline && (
+          <DateInput
+            type={"date"}
+            value={deadline}
+            onChange={(event) => {
+              setDeadline(event.target.value);
+              sendLog(LogEvents.NoticeWritingPageSetDeadline, {
+                deadline: event.target.value,
+              });
+            }}
+            onKeyDown={(event) => {
+              event.preventDefault();
+            }}
+            onClick={(event: React.MouseEvent<HTMLInputElement>) => {
+              // @ts-ignore
+              event.target.showPicker();
+            }}
+          />
+        )}
+      </Flex>
       <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
         <Checkbox />
         <Text font={Font.Medium} size="0.9rem" color={colorSet.text}>
