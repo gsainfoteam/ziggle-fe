52952e2dc2e71abcafb811ad7578877766329f82
2paperstar <lee@paperst.ar>
2023-11-02 14:20:50 +0900

    chore: use type local for temporary reason
    기존 서버랑 같이 사용하기 위해서.. 일단 이렇게 하였습니다.
    (기존 웹은 뒤에 패스를 안 받지만 지금 마이그레이션 하는 버전은 /api/login이라는 패스가 필요하기
    때문에 redirect 되는 URL이 다름)

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -3,12 +3,7 @@ import api from '..';
 export const getToken = (code: string) =>
   api
     .get<{ access_token: string }>('/user/login', {
-      params: {
-        code,
-        ...(process.env.NEXT_PUBLIC_IDP_REDIRECT_URI?.includes('localhost') && {
-          type: 'local',
-        }),
-      },
+      params: { code, type: 'local' },
     })
     .then(({ data, headers }) => {
       const refreshToken = headers['set-cookie']
