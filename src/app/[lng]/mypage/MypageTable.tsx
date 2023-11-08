74cf135d3edd247b0af688892e8ed3379945a40a
Shim su yeon <suyeonshim@gm.gist.ac.kr>
2023-11-08 22:12:21 +0900

    mypage ui edit

--- src/app/[lng]/mypage/MypageTable.tsx
+++ src/app/[lng]/mypage/MypageTable.tsx
@@ -1,4 +1,4 @@
-import LazyCat from '@/assets/lazy-cat.svg';
+import LazyCat from "@/assets/lazy-cat.svg";
 
 interface Tag {
   id: number;
@@ -27,30 +27,34 @@ interface MypageTableProps {
 }
 
 const MypageTable = ({ title, articles, link }: MypageTableProps) => {
-
   return (
-    <div className="rounded-lg shadow-md bg-white w-96">
+    <div className="rounded-lg shadow-md bg-white w-[700px]">
       <div className="w-full border border-white border-separate border-collapse border-spacing-0 rounded-lg">
-      <div className="m-0 flex flex-row w-full border border-separate border-collapse border-spacing-0 rounded-tl-lg rounded-tr-lg bg-primary">
+        <div className="m-0 flex flex-row w-full border border-separate border-collapse border-spacing-0 rounded-tl-lg rounded-tr-lg bg-primary">
           <div className="w-4/5 rounded-tl-10">
-            <div className="text-lg text-xl m-5 text-white font-bold">
+            <div className="text-xl m-5 text-white font-bold">
               {title}
             </div>
           </div>
           <div className="rounded-tr-10">
-        <div className="flex items-center justify-center text-s text-regular text-white h-full">
-            전체보기
+            <div className="flex items-center justify-center text-s text-regular text-white h-full">
+              전체보기
+            </div>
+          </div>
         </div>
-</div>
-    </div>
 
         {articles.length > 0 &&
           articles.map((articleObj, index) => {
             const isLastRow = index === articles.length - 1;
             const borderBottomRadius = isLastRow ? "10px" : "0px";
             return (
-              <div key={index} className={`flex items-center flex-row justify-between bg-colorless rounded-bl-${borderBottomRadius} rounded-br-${borderBottomRadius} border border-gray-200`}>
-                <div className={`p-10 sm:p-20 pb-0 sm:pb-0 rounded-bl-${borderBottomRadius} leading-1.5 w-70`}>
+              <div
+                key={index}
+                className={`flex items-center flex-row justify-between bg-colorless rounded-bl-${borderBottomRadius} rounded-br-${borderBottomRadius} border border-gray-200`}
+              >
+                <div
+                  className={`p-10 sm:p-20 pb-0 sm:pb-0 rounded-bl-${borderBottomRadius} leading-1.5 w-70`}
+                >
                   <div className="text-regular m-5 text-white">
                     {articleObj.title}
                   </div>
@@ -65,7 +69,7 @@ const MypageTable = ({ title, articles, link }: MypageTableProps) => {
           })}
         {articles.length === 0 && (
           <div className="flex flex-col h-full justify-center items-center p-30 bg-colorless rounded-bl-0 rounded-br-10">
-            <LazyCat className="w-35 h-35" />
+            <LazyCat className="p-5 w-50 h-50" />
             <div className="text-regular m-5 text-secondayText">
               아직 저장된 글이 없습니다. =ㅅ=
             </div>
