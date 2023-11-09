22393b656f64c00e0a9030f973fe9a9728552308
leejeongwoo <vulpes54871@gmail.com>
2023-11-09 09:07:39 +0900

    :adhesive_bandage: Formatted ResultTextZabo

--- src/app/components/templates/ResultZabo/ResultTextZabo.tsx
+++ src/app/components/templates/ResultZabo/ResultTextZabo.tsx
@@ -1,11 +1,11 @@
-import dayjs from 'dayjs';
-import { Trans } from 'react-i18next';
+import dayjs from "dayjs";
+import { Trans } from "react-i18next";
 
-import { T } from '@/app/i18next';
+import { T } from "@/app/i18next";
 
-import Chip from '../../molecules/Chip';
-import HighlightedText from '../../molecules/HighlightedText';
-import { TextZaboProps } from '../../organisms/Zabo/TextZabo';
+import Chip from "../../molecules/Chip";
+import HighlightedText from "../../molecules/HighlightedText";
+import { TextZaboProps } from "../../organisms/Zabo/TextZabo";
 
 const ResultTextZabo = ({
   title,
@@ -23,15 +23,15 @@ const ResultTextZabo = ({
   return (
     <div
       className={
-        'flex flex-col justify-between gap-2.5 p-5' +
-        'w-full box-border overflow-hidden rounded border cursor-pointer border-secondayText' +
-        'bg-white dark:bg-neutral-900'
+        "flex flex-col justify-between gap-2.5 p-5 " +
+        "w-full box-border overflow-hidden rounded border cursor-pointer border-secondayText " +
+        "bg-white dark:bg-neutral-900"
       }
     >
       <div className="flex flex-col items-start">
         <div className="font-medium text-lg mb-1.5">
           <Trans t={t} i18nKey="zabo.dueAt">
-            {{ dueAt: dayjs(deadline).format('LLLL') }}
+            {{ dueAt: dayjs(deadline).format("LLLL") }}
           </Trans>
         </div>
         <div className="font-bold text-3xl text-start">
@@ -49,22 +49,21 @@ const ResultTextZabo = ({
           {tags.map((tag, index) => (
             <Chip
               key={index}
-              variant={tag === searchQuery ? 'contained' : undefined}
+              variant={tag === searchQuery ? "contained" : undefined}
             >
-              {'#' + tag}
+              {`#${tag}`}
             </Chip>
           ))}
         </div>
 
         <div className="font-medium text-sm text-start text-ellipsis line-clamp-4">
-          {body}
-          {!body && <Trans t={t} i18nKey="zabo.noContent" />}
+          {body ?? t("zabo.noContent")}
         </div>
 
         <div className="flex gap-0.5">
           <div className="text-sm text-secondayText font-medium flex">
             <Trans t={t} i18nKey="zabo.dateView">
-              {{ date: dayjs(createdAt).format('L') }}
+              {{ date: dayjs(createdAt).format("L") }}
               <strong className="font-bold"> · {{ views }}</strong>
             </Trans>
           </div>
