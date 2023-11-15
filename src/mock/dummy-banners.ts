df56e15e15ced80b6840d5038c6bd5c2f9347e2c
Dohyun-Ko <dohyun682@gmail.com>
2023-11-15 22:28:28 +0900

    :pencil2: Fix build error

---
+++ src/mock/dummy-banners.ts
@@ -0,0 +1,20 @@
+import banner3 from '../pages/home/assets/banner_LEGACY.webp';
+import banner1 from '../pages/home/assets/banner1.png';
+import banner2 from '../pages/home/assets/banner2.png';
+
+const dummyBanners = [
+  {
+    imageUrl: banner1,
+    objectPosition: 'right',
+  },
+  {
+    imageUrl: banner2,
+    objectPosition: 'center',
+  },
+  {
+    imageUrl: banner3,
+    objectPosition: 'left',
+  },
+];
+
+export default dummyBanners;
---
+++ src/mock/dummy-mypage-articles.ts
@@ -0,0 +1,53 @@
+import MyPageInfo from './dummy-mypage-info';
+
+const articles1 = [
+  {
+    title: '23학번 과잠 구매 희망자 조사',
+    createdAt: '2023-08-03T11:57:18.740Z',
+  },
+  {
+    title: "공연동아리 지대로 연극 'ART'",
+    createdAt: '2023-08-03T11:57:18.740Z',
+  },
+  {
+    title: '2023년도 WING 신규 부원 모집!',
+    createdAt: '2023-08-03T11:57:18.740Z',
+  },
+  {
+    title:
+      '23년도 인포팀 신규 부원 모집 23년도 인포팀 신규 부원 모집 23년도 인포팀 신규 부원 모집 23년도 인포팀 신규 부원 모집 23년도 인포팀 신규 부원 모집 23년도 인포팀 신규 부원 모집',
+    createdAt: '2023.02.13.',
+  },
+].map((article) => ({
+  ...article,
+  author: MyPageInfo.name,
+  body: '',
+  deadline: '',
+  id: 0,
+  imageUrl: null,
+  tags: [],
+  views: 0,
+}));
+
+const articles2 = [
+  {
+    title: '23학번 과잠 구매 희망자 조사',
+    createdAt: '2023-08-03T11:57:18.740Z',
+  },
+].map((article) => ({
+  ...article,
+  author: MyPageInfo.name,
+  body: '',
+  deadline: '',
+  id: 0,
+  imageUrl: null,
+  tags: [],
+  views: 0,
+}));
+
+const dummyMypageArticles = {
+  articles1,
+  articles2,
+};
+
+export default dummyMypageArticles;
