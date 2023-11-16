08187188778b66bba1ac6bb9a75deabb0b69967b
2paperstar <lee@paperst.ar>
2023-11-16 15:10:09 +0900

    fix: make list page dynamic

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -4,6 +4,8 @@ import ZaboCarousel from '../components/templates/ZaboCarousel';
 import { createTranslation } from '../i18next';
 import { Locale } from '../i18next/settings';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home({
   params: { lng },
 }: {
