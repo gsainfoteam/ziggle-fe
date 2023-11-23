755e0f4b9891cb90800e6b9c895e91357579f709
2paperstar <lee@paperst.ar>
2023-11-23 15:35:15 +0900

    feat: p tag margin in content

--- src/app/[lng]/notice/[id]/Content.tsx
+++ src/app/[lng]/notice/[id]/Content.tsx
@@ -9,7 +9,7 @@ const Content = ({ content }: ContentProps) => {
     <div
       className={[
         'font-normal leading-6',
-        '[&_p]:text-base md:[&_p]:text-lg',
+        '[&_p]:my-4 [&_p]:text-base md:[&_p]:text-lg',
         '[&_h1]:text-2xl md:[&_h1]:text-4xl',
         '[&_h2]:text-xl md:[&_h2]:text-2xl',
         '[&_h3]:text-lg md:[&_h3]:text-xl',
