e4841782756ec379d3d113db37cbd7934637352d
2paperstar <lee@paperst.ar>
2024-01-10 21:07:13 +0900

    chore: redirect [wip]

--- src/app/[lng]/(empty)/app/page.tsx
+++ src/app/[lng]/(empty)/app/page.tsx
@@ -1,8 +1,26 @@
+'use server';
+
 import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+import {
+  appStoreLink,
+  playStoreLink,
+} from '@/app/components/templates/Footer/Footer';
 
 const AppOpenPage = () => {
   const h = headers();
-  return <>{JSON.stringify(h)}</>;
+  const userAgent = h.get('user-agent');
+  const isAndroid = Boolean(userAgent?.match(/Android/i));
+  const isIos = Boolean(userAgent?.match(/iPhone|iPad|iPod/i));
+  const host = h.get('host');
+  const protocol = h.get('x-forwarded-proto') || 'https';
+  const origin = `${protocol}://${host}`;
+
+  if (!isAndroid && !isIos) redirect(origin);
+  if (isAndroid) redirect(playStoreLink);
+  if (isIos) redirect(appStoreLink);
+  redirect(origin);
 };
 
 export default AppOpenPage;
