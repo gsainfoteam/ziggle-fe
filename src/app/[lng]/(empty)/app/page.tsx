ddf1e3af4fd50f2db76f1fb5685ea2e361655453
2paperstar <lee@paperst.ar>
2024-01-12 17:13:33 +0900

    feat: open app redirect or show store page

--- src/app/[lng]/(empty)/app/page.tsx
+++ src/app/[lng]/(empty)/app/page.tsx
@@ -8,7 +8,12 @@ import {
   playStoreLink,
 } from '@/app/components/templates/Footer/Footer';
 
-const AppOpenPage = () => {
+const AppOpenPage = ({
+  searchParams,
+}: {
+  searchParams: { redirect: string; install: string };
+}) => {
+  console.log(searchParams);
   const h = headers();
   const userAgent = h.get('user-agent');
   const isAndroid = Boolean(userAgent?.match(/Android/i));
@@ -17,10 +22,15 @@ const AppOpenPage = () => {
   const protocol = h.get('x-forwarded-proto') || 'https';
   const origin = `${protocol}://${host}`;
 
+  if (searchParams.install === 'true') {
+    if (isAndroid) redirect(playStoreLink);
+    if (isIos) redirect(appStoreLink);
+  }
   if (!isAndroid && !isIos) redirect(origin);
-  if (isAndroid) redirect(playStoreLink);
-  if (isIos) redirect(appStoreLink);
-  redirect(origin);
+  const url = new URL('https://ziggle.gistory.me/app');
+  url.searchParams.set('redirect', searchParams.redirect);
+  url.searchParams.set('install', 'true');
+  redirect(url.toString());
 };
 
 export default AppOpenPage;
