359fdbd61c0a45696870d3c00328c77bc1ffb9cf
2paperstar <lee@paperst.ar>
2024-03-06 23:24:01 +0900

    fix: remove apollo client in InitClient.tsx

--- src/app/[lng]/(common)/InitClient.tsx
+++ src/app/[lng]/(common)/InitClient.tsx
@@ -2,7 +2,6 @@
 
 import '@/app/initDayjs';
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 import { SWRConfig } from 'swr';
@@ -10,40 +9,6 @@ import { SWRConfig } from 'swr';
 import api from '@/api';
 
 import { PropsWithLng } from '../../i18next';
-
-const apolloCache = new InMemoryCache({
-  typePolicies: {
-    Content: {
-      keyFields: false,
-    },
-    Query: {
-      fields: {
-        notices: {
-          keyArgs: false,
-          merge: (existing, incoming, { args }) => {
-            const merged = existing ? existing.list.slice(0) : [];
-            if (incoming) {
-              if (args) {
-                const { offset = 0 } = args;
-                for (let i = 0; i < incoming.list.length; ++i) {
-                  merged[offset + i] = incoming.list[i];
-                }
-              } else {
-                merged.push.apply(merged, incoming.list);
-              }
-            }
-            return { ...incoming, list: merged };
-          },
-        },
-      },
-    },
-  },
-});
-
-export const apolloClient = new ApolloClient({
-  uri: '/api/graphql',
-  cache: apolloCache,
-});
 
 const InstallApp = dynamic(() => import('./InstallApp'), {
   ssr: false,
@@ -56,12 +21,10 @@ const InitClient = ({
   <SWRConfig
     value={{ fetcher: (path) => api.get(path).then(({ data }) => data) }}
   >
-    <ApolloProvider client={apolloClient}>
-      <Suspense>
-        <InstallApp lng={lng} />
-      </Suspense>
-      {children}
-    </ApolloProvider>
+    <Suspense>
+      <InstallApp lng={lng} />
+    </Suspense>
+    {children}
   </SWRConfig>
 );
 
