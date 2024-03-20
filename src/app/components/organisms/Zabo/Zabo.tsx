c9d4af9196abedbfd78c3d482c335798b055aa21
Do-hyun Ko <dohyun682@gmail.com>
2024-03-20 16:21:08 +0900

    :bug: Fix type errors

--- src/app/components/organisms/Zabo/Zabo.tsx
+++ src/app/components/organisms/Zabo/Zabo.tsx
@@ -11,7 +11,18 @@ import ZaboActions from './ZaboActions';
 import ZaboTags from './ZaboTags';
 dayjs.extend(relativeTime); // do not work within initClient
 
-export type ZaboProps = PropsWithT<Notice>;
+export type ZaboOrigin = 'width' | 'height';
+
+export type ZaboSize<Origin extends ZaboOrigin> = Origin extends 'width'
+  ? { width: number; height?: never }
+  : Origin extends 'height'
+    ? { height: number; width?: never }
+    : never;
+
+export type ZaboProps = PropsWithT<Notice> & {
+  width?: number;
+  height?: number; // migration ongoing | remove after migration complete
+};
 
 const Zabo = (props: ZaboProps & PropsWithLng) => {
   const { createdAt, author, deadline, reactions, t, title, imageUrls, tags } =
