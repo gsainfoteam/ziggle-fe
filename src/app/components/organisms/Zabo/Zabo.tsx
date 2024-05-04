24d90bd66600c89897792be49896d9f9f347633c
Do-hyun Ko <dohyun682@gmail.com>
2024-05-04 23:57:48 +0900

    :bug: Fix zabo first image cropped in muliple-image zabo bug

--- src/app/components/organisms/Zabo/Zabo.tsx
+++ src/app/components/organisms/Zabo/Zabo.tsx
@@ -48,19 +48,21 @@ const Zabo = (props: ZaboProps & PropsWithLng) => {
       {!hasImage && <ZaboTags notice={props} />}
 
       {hasImage && (
-        <div className={'flex justify-center gap-[10px] overflow-x-scroll'}>
-          {imageUrls.map((url) => (
-            <Image
-              key={url}
-              src={url}
-              alt={title}
-              width={200}
-              height={200}
-              className={
-                'h-[200px] w-[200px] rounded-[5px] border border-gray-300 object-cover'
-              }
-            />
-          ))}
+        <div className={'flex justify-center'}>
+          {/* // 아래 div에 바로 justify-center를 적용하면, 1번 2번 이미지가 잘려서
+          보임 */}
+          <div className={'flex flex-nowrap gap-[10px] overflow-x-scroll'}>
+            {imageUrls.map((url) => (
+              <Image
+                key={url}
+                src={url}
+                alt={title}
+                width={200}
+                height={200}
+                className={'rounded-[5px] border border-gray-300 object-cover'}
+              />
+            ))}
+          </div>
         </div>
       )}
 
