e88afd946451d562f6d2cd37ae6308b96ded5305
leejeongwoo <vulpes54871@gmail.com>
2024-05-05 13:43:05 +0900

    removed comment

--- src/app/[lng]/(common)/page.tsx
+++ src/app/[lng]/(common)/page.tsx
@@ -13,15 +13,6 @@ export default async function Home({
 }) {
   const { t } = await createTranslation(lng);
 
-  // const [deadline, hot, event, recruit, general, academic] = await Promise.all([
-  //   getAllNotices({ orderBy: 'deadline' }),
-  //   getAllNotices({ orderBy: 'hot' }),
-  //   getAllNotices({ tags: ['event'], orderBy: 'recent' }),
-  //   getAllNotices({ tags: ['recruit'], orderBy: 'recent' }),
-  //   getAllNotices({ tags: ['general'], orderBy: 'recent' }),
-  //   getAllNotices({ tags: ['academic'], orderBy: 'recent' }),
-  // ]);
-
   const recentNotices = await getAllNotices({ orderBy: 'recent' });
 
   return (
@@ -37,50 +28,6 @@ export default async function Home({
           ))}
         </div>
       </div>
-
-      {/* <ZaboCarousel
-        notices={deadline.list}
-        title={t('notices.deadline.label')}
-        height={300}
-        carouselClassName="bg-primary/10"
-        sectionHref="/section/urgent"
-        lng={lng}
-      />
-      <ZaboCarousel
-        notices={hot.list}
-        title={t('notices.hot.label')}
-        height={300}
-        sectionHref="/section/hot"
-        lng={lng}
-      />
-      <ZaboCarousel
-        notices={event.list}
-        title={t('notices.event.label')}
-        height={300}
-        sectionHref="/section/event"
-        lng={lng}
-      />
-      <ZaboCarousel
-        notices={recruit.list}
-        title={t('notices.recruit.label')}
-        height={300}
-        sectionHref="/section/recruit"
-        lng={lng}
-      />
-      <ZaboCarousel
-        notices={general.list}
-        title={t('notices.general.label')}
-        height={300}
-        sectionHref="/section/general"
-        lng={lng}
-      />
-      <ZaboCarousel
-        notices={academic.list}
-        title={t('notices.academic.label')}
-        height={300}
-        sectionHref="https://www.gist.ac.kr/kr/html/sub05/050209.html"
-        lng={lng}
-      /> */}
     </main>
   );
 }
