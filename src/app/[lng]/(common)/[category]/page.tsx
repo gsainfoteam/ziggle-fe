8973fd945d29b5715169f4cbd432562acf718ac7
ParkJumyung <parkjumyungpublic@gmail.com>
2024-05-15 22:54:08 +0900

    ✨ implement category page order by deadline

--- src/app/[lng]/(common)/[category]/page.tsx
+++ src/app/[lng]/(common)/[category]/page.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
+
 import { getAllNotices } from '@/api/notice/notice-server';
 import Toggle from '@/app/components/atoms/Toggle/Toggle';
+import styles from '@/app/components/atoms/Toggle/toggle.module.css';
 import Zabo from '@/app/components/organisms/Zabo';
 import { sidebarObject } from '@/app/components/templates/Sidebar/sidebarObject';
 
@@ -10,27 +13,54 @@ export const dynamic = 'force-dynamic';
 
 export default async function Home({
   params: { lng, category },
+  searchParams,
 }: {
   params: PropsWithLng & {
     category: HomePath;
   };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }) {
   const { t } = await createTranslation(lng);
 
+  const sortByDeadline = searchParams?.deadline === 'true' ?? false;
+
   const { noticeSearchParams, icons, title } =
     sidebarObject.flat(2).find(({ path }) => path === category) ??
-    sidebarObject[0][0]; // default to home
-  const notices = await getAllNotices(noticeSearchParams);
-
-  console.log(noticeSearchParams);
+    sidebarObject[0][0]; // default to first menu(home)
+  const notices = await getAllNotices(
+    sortByDeadline
+      ? { ...noticeSearchParams, orderBy: 'deadline' }
+      : noticeSearchParams,
+  );
 
   return (
     <main className="flex w-full flex-col items-center gap-5">
       <div className="flex w-full flex-col items-center">
         {category !== 'home' && (
-          <div className="flex w-full flex-row items-center gap-2 py-5 text-4xl font-bold text-primary md:max-w-[800px]">
-            <icons.bold className="fill-primary stroke-text dark:stroke-none" />
-            {t(title)}
+          <div className="flex w-full flex-row justify-between py-5 font-bold md:max-w-[800px]">
+            <div className="flex items-center gap-2 text-4xl text-primary ">
+              <icons.bold className="fill-primary stroke-text dark:stroke-none" />
+              {t(title)}
+            </div>
+            <div
+              className={`flex items-center gap-3 text-lg font-medium ${
+                sortByDeadline
+                  ? 'text-primary'
+                  : 'text-greyDark dark:text-greyLight'
+              }`}
+            >
+              <Link
+                href={`${category}${sortByDeadline ? '' : '?deadline=true'}`}
+                className="flex rounded-full"
+              >
+                <input
+                  className={styles.checkbox}
+                  type="checkbox"
+                  checked={sortByDeadline}
+                />
+              </Link>
+              마감임박 순으로 보기
+            </div>
           </div>
         )}
         <div className="flex w-full flex-col md:max-w-[800px]">
