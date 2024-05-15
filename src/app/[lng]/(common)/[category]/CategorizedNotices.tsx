c1f32f261f13ad237d303fb5416070f8a90af0ee
ParkJumyung <parkjumyungpublic@gmail.com>
2024-05-16 00:48:31 +0900

    ✨ show something when no notices in category

--- src/app/[lng]/(common)/[category]/CategorizedNotices.tsx
+++ src/app/[lng]/(common)/[category]/CategorizedNotices.tsx
@@ -2,7 +2,8 @@ import { NoticeSearchParams } from '@/api/notice/notice';
 import { getAllNotices } from '@/api/notice/notice-server';
 import Pagination from '@/app/components/molecules/Pagination';
 import Zabo from '@/app/components/organisms/Zabo';
-import { PropsWithLng } from '@/app/i18next';
+import { createTranslation, PropsWithLng } from '@/app/i18next';
+import SearchNoResult from '@/assets/search-no-result.svg';
 
 interface CategorizedNoticesProps {
   sortByDeadline: boolean;
@@ -16,6 +17,8 @@ const CategorizedNotices = async ({
   lng,
   page,
 }: PropsWithLng<CategorizedNoticesProps>) => {
+  const { t } = await createTranslation(lng);
+
   const notices = await getAllNotices(
     sortByDeadline
       ? { ...noticeSearchParams, orderBy: 'deadline' }
@@ -24,15 +27,32 @@ const CategorizedNotices = async ({
 
   return (
     <>
-      <div className="flex w-full flex-col md:max-w-[800px]">
-        {...notices.list.map((notice) => (
-          <>
-            <Zabo key={notice.id} {...notice} lng={lng} />
-            <div className="my-[60px] h-[1px] bg-greyLight dark:bg-dark_greyBorder" />
-          </>
-        ))}
-      </div>
-      <Pagination page={page} items={notices.total} itemsPerPage={30} />
+      {notices.list.length ? (
+        <>
+          <div className="flex w-full flex-col md:max-w-[800px]">
+            {...notices.list.map((notice) => (
+              <>
+                <Zabo key={notice.id} {...notice} lng={lng} />
+                <div className="my-[60px] h-[1px] bg-greyLight dark:bg-dark_greyBorder" />
+              </>
+            ))}
+          </div>
+          <Pagination page={page} items={notices.total} itemsPerPage={30} />
+        </>
+      ) : (
+        <div className="flex w-full justify-center">
+          <div className="align-center flex flex-col justify-center">
+            <div className="h-[100px]" />
+            <div style={{ height: '10px', margin: '0 auto' }}></div>
+
+            <SearchNoResult />
+
+            <p className="font-lg md:font-2xl pt-5 text-center font-bold text-secondaryText">
+              {t('emptyNotices')}
+            </p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
