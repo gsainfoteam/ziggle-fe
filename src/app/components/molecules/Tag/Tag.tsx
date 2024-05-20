0ff00674bef914f0e96533fedd3280b1effbe2ed
2paperstar <lee@paperst.ar>
2024-05-20 14:04:04 +0900

    fix: missing lng in tags

--- src/app/components/molecules/Tag/Tag.tsx
+++ src/app/components/molecules/Tag/Tag.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link';
 
+import { PropsWithLng } from '@/app/i18next';
+
 interface TagProps {
   name: string;
 }
 
-const Tag = ({ name }: TagProps) => {
+const Tag = ({ lng, name }: PropsWithLng<TagProps>) => {
   return (
     <Link
-      href={`/section/${name}`}
+      href={`${lng}/section/${name}`}
       className={
         'rounded-[5px] bg-secondary px-[10px] py-[5px] text-lg text-primary dark:bg-dark_secondary'
       }
