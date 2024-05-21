c924447dc13150aff9695f11d67f23bf6ca59317
ParkJumyung <parkjumyungpublic@gmail.com>
2024-05-21 18:04:24 +0900

    ✨ temporary default time picker

--- src/app/components/organisms/DatetimePicker/DatetimePicker.tsx
+++ src/app/components/organisms/DatetimePicker/DatetimePicker.tsx
@@ -8,13 +8,20 @@ import React from 'react';
 import { useState } from 'react';
 import DatePicker from 'react-date-picker';
 
+import TimePicker from './TimePicker';
+
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const DateTimePicker = () => {
   const [date, onDateChange] = useState<Value>(dayjs().toDate());
 
-  return <DatePicker value={date} onChange={onDateChange} />;
+  return (
+    <div className="flex">
+      <DatePicker value={date} onChange={onDateChange} />
+      <TimePicker />
+    </div>
+  );
 };
 
 export default DateTimePicker;
---
+++ src/app/components/organisms/DatetimePicker/TimePicker.tsx
@@ -0,0 +1,10 @@
+const TimePicker = () => {
+  return (
+    <input
+      type="time"
+      className="flex items-center justify-center rounded-r-md bg-primary py-1 pr-2 font-semibold outline-none hover:cursor-pointer"
+    />
+  );
+};
+
+export default TimePicker;
