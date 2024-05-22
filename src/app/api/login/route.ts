2e6030e48b0bcff3a0410d85b63061f0f867d036
2paperstar <lee@paperst.ar>
2024-05-23 03:52:57 +0900

    fix: idp host

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -49,6 +49,7 @@ export const GET = async (request: NextRequest) => {
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 6),
     });
     const url = request.nextUrl.clone();
+    url.host = request.headers.get('host')!;
     url.pathname = '/';
     url.search = '';
     return NextResponse.redirect(url);
