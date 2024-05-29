a0e47766ed51c3210e6da165de1b4f3dbf08a7fc
2paperstar <lee@paperst.ar>
2024-05-29 23:27:42 +0900

    feat: support english dayjs

--- src/app/initDayjs.ts
+++ src/app/initDayjs.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import en from 'dayjs/locale/en';
 import ko from 'dayjs/locale/ko';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -8,6 +9,7 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(LocalizedFormat);
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.locale(en);
 dayjs.locale(ko);
 dayjs.tz.setDefault('Asia/Seoul');
 dayjs.extend(relativeTime);
