964ab4a9b632765489b2ba9b12d228665356992c
Hongje Choi <enc2586@gmail.com>
2024-07-23 16:54:31 +0900

    fix: divided Toggle element's props into 'isSwitched', 'defaultIsSwitched'

--- src/app/components/atoms/Toggle/Toggle.tsx
+++ src/app/components/atoms/Toggle/Toggle.tsx
@@ -4,17 +4,19 @@ import styles from './toggle.module.css';
 
 interface ToggleProps {
   isSwitched: boolean;
+  defaultIsSwitched?: boolean;
   onSwitch: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Toggle = ({ isSwitched, onSwitch }: ToggleProps) => {
+const Toggle = ({ isSwitched, defaultIsSwitched, onSwitch }: ToggleProps) => {
   return (
     <label className={styles.wrapper}>
       <input
         role="switch"
         type="checkbox"
         className={styles.checkbox}
-        defaultChecked={isSwitched}
+        checked={isSwitched}
+        defaultChecked={defaultIsSwitched}
         onChange={onSwitch}
       />
     </label>
