fb18a75f818dc999fc99470c84b57ad8d0010541
ParkJumyung <parkjumyungpublic@gmail.com>
2024-09-03 14:15:57 +0900

    ♻️ refactor in progress...

--- src/app/[lng]/(common)/(common)/layout.tsx
+++ src/app/[lng]/(common)/(common)/layout.tsx
@@ -1,13 +1,10 @@
-import { createTranslation, PropsWithLng } from '@/app/i18next';
+import { PropsWithLng } from '@/app/i18next';
 
 export default async function Layout({
   children,
-  params: { lng },
 }: {
   children: React.ReactNode;
   params: PropsWithLng;
 }) {
-  const { t } = await createTranslation(lng);
-
   return <div className="w-0 grow md:mx-5">{children}</div>;
 }
--- src/app/[lng]/(common)/(needSidebar)/[category]/page.tsx
+++ src/app/[lng]/(common)/(needSidebar)/[category]/page.tsx
@@ -12,26 +12,27 @@ import { HomePath } from './paths';
 
 export const dynamic = 'force-dynamic';
 
-export default async function Home({
-  params: { lng, category },
-  searchParams,
-}: {
+interface CategoryPageProps {
   params: PropsWithLng & {
     category: HomePath;
   };
   searchParams?: { deadline: 'true' | 'false'; page: string };
-}) {
+}
+
+export default async function CategoryPage({
+  params: { lng, category },
+  searchParams,
+}: CategoryPageProps) {
   const { t } = await createTranslation(lng);
 
   const sortByDeadline = searchParams?.deadline === 'true' ?? false;
   const page = parseInt(searchParams?.page ?? '');
-
-  if (Number.isNaN(page) || page < 0) {
+  const isPageValid = !Number.isNaN(page) && page >= 0;
+  if (!isPageValid) {
     redirect(
       `/${lng}/${category}?page=0${sortByDeadline ? '&deadline=true' : ''}`,
     );
   }
-
   const currentSidebarObject = sidebarObject
     .flat(2)
     .find(({ path }) => path === category);
@@ -47,7 +48,7 @@ export default async function Home({
           <div className="flex w-full flex-row flex-wrap justify-between gap-[14px] px-[18px] py-5 font-bold md:max-w-[800px]">
             <div className="flex items-center gap-2 text-4xl text-primary ">
               <icons.bold className="w-10 fill-primary stroke-white dark:stroke-dark_dark" />
-              {t(title) as string}
+              {t(title)}
             </div>
             {category !== 'deadline' && category !== 'zigglepick' && (
               <div className={`flex items-center gap-3`}>
