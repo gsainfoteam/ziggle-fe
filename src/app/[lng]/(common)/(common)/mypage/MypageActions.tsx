5dcf7562cd04d348a1143194e40526af0c9fbd8e
Hongje Choi <enc2586@gmail.com>
2024-09-30 15:04:06 +0900

    Refactor MypageActions component and remove unused code

--- src/app/[lng]/(common)/(common)/mypage/MypageActions.tsx
+++ src/app/[lng]/(common)/(common)/mypage/MypageActions.tsx
@@ -1,15 +1,10 @@
-import { cookies } from 'next/headers';
-
 import { PropsWithLng } from '@/app/i18next';
 
-import ChangeDarkModeBox, { ColorThemeCookie } from './ChangeDarkModeBox';
+import ChangeDarkModeBox from './ChangeDarkModeBox';
 import ChangeLanguageBox from './ChangeLanguageBox';
 import ClientActions from './ClientActions';
 
 export default function MypageActions({ lng }: PropsWithLng) {
-  const cookieStore = cookies();
-  const theme = cookieStore.get('theme') as ColorThemeCookie | undefined;
-
   return (
     <div className="flex flex-col gap-3">
       <ChangeLanguageBox lng={lng} />
