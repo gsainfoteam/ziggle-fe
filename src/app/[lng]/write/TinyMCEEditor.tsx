2f21e1871a4beda28b4ba8c5498057e7ef26226b
ParkJumyung <parkjumyungpublic@gmail.com>
2024-10-04 22:51:54 +0900

    🔀 resolve merge conflict

--- src/app/[lng]/write/TinyMCEEditor.tsx
+++ src/app/[lng]/write/TinyMCEEditor.tsx
@@ -30,42 +30,43 @@ const TinyMCEEditor = ({
   editorRef,
   initialValue,
   disabled,
-}: TinyMCEEditorProps) => {
-  return (
-    <Editor
-      disabled={disabled}
-      onInit={(_, editor) => {
-        if (!editorRef) return;
-        if (typeof editorRef === 'function') editorRef(editor);
-        else editorRef.current = editor;
-      }}
-      initialValue={initialValue}
-      value={value}
-      onEditorChange={(newValue, _) => onChange(newValue)}
-      tinymceScriptSrc="/tinymce/tinymce.min.js"
-      init={{
-        skin: window.matchMedia('(prefers-color-scheme: dark)').matches
-          ? 'oxide-dark'
-          : 'oxide',
-        content_css: window.matchMedia('(prefers-color-scheme: dark)').matches
-          ? 'dark'
-          : 'default',
-        promotion: false,
-        plugins: ['link', 'image', 'code', 'autolink'],
-        linkchecker_service_url: '/linkchecker',
-        toolbar:
-          'undo redo | formatselect | ' +
-          'bold italic backcolor | alignleft aligncenter ' +
-          'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | link',
-      }}
-      onBlur={(event) =>
-        sendLog(LogEvents.noticeWritingPageTypeContent, {
-          content: event.target.getContent(),
-        })
-      }
-    />
-  );
-};
+}: TinyMCEEditorProps) => (
+  <Editor
+    disabled={disabled}
+    onInit={(_, editor) => {
+      if (!editorRef) return;
+      if (typeof editorRef === 'function') editorRef(editor);
+      else editorRef.current = editor;
+    }}
+    initialValue={initialValue}
+    value={value}
+    onEditorChange={(newValue, _) => onChange(newValue)}
+    tinymceScriptSrc="/tinymce/tinymce.min.js"
+    init={{
+      skin: window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'oxide-dark'
+        : 'oxide',
+      content_css: window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'default',
+      promotion: false,
+      plugins: ['link', 'image', 'code', 'autolink'],
+      linkchecker_service_url: '/linkchecker',
+      toolbar:
+        'undo redo | formatselect | ' +
+        'bold italic backcolor | alignleft aligncenter ' +
+        'alignright alignjustify | bullist numlist outdent indent | ' +
+        'removeformat | link',
+      link_target_list: [
+        { title: 'New page', value: '_blank', selected: true },
+      ],
+    }}
+    onBlur={(event) =>
+      sendLog(LogEvents.noticeWritingPageTypeContent, {
+        content: event.target.getContent(),
+      })
+    }
+  />
+);
 
 export default TinyMCEEditor;
