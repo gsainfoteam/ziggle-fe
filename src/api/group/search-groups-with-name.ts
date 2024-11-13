c593325437dab2f604e24b31db5fa6394058923e
ParkJumyung <parkjumyungpublic@gmail.com>
2024-11-13 23:03:57 +0900

    ⚰️ remove mock response

--- src/api/group/search-groups-with-name.ts
+++ src/api/group/search-groups-with-name.ts
@@ -10,15 +10,6 @@ export interface GroupInfo {
 }
 
 export const serachGroupsWithName = async (groupName: string, lang: Locale) => {
-  return [
-    {
-      uuid: '1cff6b40-5db2-45de-bb59-0825267052b3',
-      name: 'realziggle',
-      verified: true,
-      profileImageUrl: null,
-    },
-  ];
-
   return await ziggleApi
     .get<GroupInfo[]>(`/groups/search?name=${groupName}&lang=${lang}`)
     .then(({ data }) => data);
