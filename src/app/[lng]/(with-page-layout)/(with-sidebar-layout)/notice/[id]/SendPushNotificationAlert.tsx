ee16defadd6ef0a7a704a64e3b4e1e6aa77cb142
Hongje Choi <enc2586@gmail.com>
2024-11-14 17:02:01 +0900

    feature: send alarm

--- src/app/[lng]/(with-page-layout)/(with-sidebar-layout)/notice/[id]/SendPushNotificationAlert.tsx
+++ src/app/[lng]/(with-page-layout)/(with-sidebar-layout)/notice/[id]/SendPushNotificationAlert.tsx
@@ -2,34 +2,68 @@
 
 import { useSession } from 'next-auth/react';
 import { useCallback, useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 
 import { NoticeDetail } from '@/api/notice/notice';
+import { sendNoticeAlarm } from '@/api/notice/send-alarm';
 import { calculateRemainingTime } from '@/app/[lng]/write/calculateRemainingTime';
 import { PropsWithLng } from '@/app/i18next';
 import { useTranslation } from '@/app/i18next/client';
 
 interface SendPushNotificationAlertProps
-  extends Pick<NoticeDetail, 'author' | 'createdAt'> {}
+  extends Pick<NoticeDetail, 'id' | 'author' | 'publishedAt'> {}
 
 const SendPushNotificationAlert = ({
+  id,
   author,
-  createdAt,
+  publishedAt,
   lng,
 }: PropsWithLng<SendPushNotificationAlertProps>): JSX.Element | null => {
   const { t } = useTranslation(lng);
 
-  const handleSendPushNotification = useCallback(() => {
-    // TODO: Implement push notification sending
-  }, []);
+  const handleSendPushNotification = useCallback(async () => {
+    const result = await Swal.fire({
+      text: t('write.alerts.sendPushNotice'),
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: t('alertResponse.confirm'),
+      cancelButtonText: t('alertResponse.cancel'),
+    });
+
+    if (!result.isConfirmed) return;
+
+    Swal.fire({
+      text: t('write.alerts.sendingAlarmNotice'),
+      icon: 'info',
+      showConfirmButton: false,
+      allowOutsideClick: false,
+    });
+
+    const newNotice = await sendNoticeAlarm({ id }).catch(() => null);
+
+    if (!newNotice) {
+      Swal.fire({
+        text: t('write.alerts.sendPushNoticeFail'),
+        icon: 'error',
+        confirmButtonText: t('alertResponse.confirm'),
+      });
+      return;
+    }
+
+    // update notice data with newNotice or refetch it
+    // so that SendPushNotificationAlert will be hidden
+  }, [t, id]);
 
   const { data: user } = useSession();
   const isMyNotice = user?.user.uuid === author.uuid;
 
-  const [timeRemaining, setTimeRemaining] = useState(calculateRemainingTime(createdAt));
+  const [timeRemaining, setTimeRemaining] = useState(
+    calculateRemainingTime(publishedAt),
+  );
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimeRemaining(calculateRemainingTime(createdAt));
+      setTimeRemaining(calculateRemainingTime(publishedAt));
     }, 1000);
 
     return () => clearInterval(interval);
