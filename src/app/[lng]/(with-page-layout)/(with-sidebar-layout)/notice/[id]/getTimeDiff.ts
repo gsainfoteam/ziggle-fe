ba2672e172b2fd93d38b994af2fcf4a2c41f3216
Hongje Choi <enc2586@gmail.com>
2024-11-18 15:13:02 +0900

    Update getTimeDiff.ts

--- src/app/[lng]/(with-page-layout)/(with-sidebar-layout)/notice/[id]/getTimeDiff.ts
+++ src/app/[lng]/(with-page-layout)/(with-sidebar-layout)/notice/[id]/getTimeDiff.ts
@@ -1,10 +1,12 @@
 import dayjs, { Dayjs } from 'dayjs';
 
 export const isServer = typeof window === 'undefined';
+
+const CLIENT_SERVER_TIME_OFFSET_SECONDS = 10
 export const getTimeDiff = (createdAt: Dayjs | string) => {
   const currentTime = dayjs();
   const diffInSeconds = dayjs(createdAt)
-    .subtract(10, 'second')
+    .subtract(CLIENT_SERVER_TIME_OFFSET_SECONDS, 'second')
     .diff(currentTime, 'second');
   const minutes = Math.floor(diffInSeconds / 60);
   const seconds = diffInSeconds % 60;
