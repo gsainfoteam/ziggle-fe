b30f4283c52b07b95cbfcce48cebce02786d7e0b
Hongje Choi <enc2586@gmail.com>
2024-11-20 22:45:15 +0900

    add: Icons in component

--- src/app/[lng]/(with-page-layout)/(without-sidebar-layout)/mypage/ChangeDarkModeBox.tsx
+++ src/app/[lng]/(with-page-layout)/(without-sidebar-layout)/mypage/ChangeDarkModeBox.tsx
@@ -6,6 +6,9 @@ import LogEvents from '@/api/log/log-events';
 import sendLog from '@/api/log/send-log';
 import { PropsWithLng } from '@/app/i18next';
 import { useTranslation } from '@/app/i18next/client';
+import DarkModeIcon from '@/assets/icons/half-moon.svg';
+import LightModeIcon from '@/assets/icons/sun-light.svg';
+import SystemModeIcon from '@/assets/icons/system-outlined.svg';
 
 import MypageBox from './MypageBox';
 
@@ -19,6 +22,11 @@ const ChangeDarkModeBox = ({ lng }: PropsWithLng) => {
       <div className="flex justify-between self-stretch">
         <div className="flex text-greyDark dark:text-dark_white">
           {t('mypage.darkModeSettings')}
+        </div>
+        <div className="flex items-center gap-4">
+          <DarkModeIcon className="h-6" />
+          <LightModeIcon className="h-6" />
+          <SystemModeIcon className="h-6" />
         </div>
         {/* <Segmented
           options={[
