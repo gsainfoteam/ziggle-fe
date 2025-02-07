344c9af11f9af62f7799e063a49e22cc84487cb0
Dohyun Ko <dohyun682@gmail.com>
2025-02-08 01:50:33 +0900

    feat: getMyGroups & remove unused vapor code

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { auth } from './auth/auth';
 
 const ziggleApi = axios.create({ baseURL: '/api/bff' });
-const vaporApi = axios.create({ baseURL: '/api/vapor-bff' });
 
 ziggleApi.interceptors.request.use(async (config) => {
   if (typeof window !== 'undefined') return config;
@@ -12,17 +11,8 @@ ziggleApi.interceptors.request.use(async (config) => {
   const session = await auth();
   if (!session) return config;
   config.headers.Authorization = `Bearer ${session.accessToken}`;
+
   return config;
 });
 
-vaporApi.interceptors.request.use(async (config) => {
-  if (typeof window !== 'undefined') return config;
-
-  config.baseURL = process.env.NEXT_VAPOR_API_URL!;
-  const session = await auth();
-  if (!session) return config;
-  config.headers.Authorization = `Bearer ${session.accessToken}`;
-  return config;
-});
-
-export { vaporApi, ziggleApi };
+export { ziggleApi };
