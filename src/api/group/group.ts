3f90572fc3564f3b5182597571589f4a03caad17
SHIM-JINSEO <sjs011828@gmail.com>
2025-08-06 12:11:52 +0900

    fix: fix third party path and add getGroupsToken API url

--- src/api/group/group.ts
+++ src/api/group/group.ts
@@ -21,22 +21,25 @@ export interface InviteCode {
 }
 
 const groupsBaseUrl = process.env.NEXT_PUBLIC_GROUPS_API_URL;
-const clientId = process.env.GROUPS_CLIENT_ID;
+const clientId = process.env.NEXT_PUBLIC_GROUPS_CLIENT_ID;
 const clientSecret = process.env.GROUPS_CLIENT_SECRET;
-const basedURL = process.env.GROUPS_REDIRECT_BASE_URI;
+const basedURL = process.env.NEXT_PUBLIC_GROUPS_REDIRECT_BASE_URL;
 export const thirdPartyAuth = async (path: string) => {
   const { data } = await axios.get(
-    `${groupsBaseUrl}/third-party/auth?client_id=${clientId}&redirect_url=${basedURL}${path}`,
+    `${groupsBaseUrl}third-party/authorize?client_id=${clientId}&redirect_uri=${basedURL}${path}`,
   );
 };
 
-export const getGroupsToken = async () => {
+export const getGroupsToken = async (code: string, path: string) => {
+  const { data } = await axios.get(
+    `${groupsBaseUrl}third-party/token/authorize?client_id=${clientId}&code=${code}&redirect_uri=${basedURL}${path}&`,
+  );
   return;
 };
 
 export const getMyGroups = async () => {
   return;
-}
+};
 
 export const getGroup = async (uuid: string): Promise<GroupInfo> => {
   return ziggleApi.get<GroupInfo>(`/group/${uuid}`).then(({ data }) => data);
