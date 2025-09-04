60c49cc007af5d4b807f44fa2adb127e6d0b969b
Hyunseung Ryu <rhseungg@gmail.com>
2025-09-04 20:40:34 +0900

    feat: react-hot-toast to sonner

--- src/app/components/layout/InitClient.tsx
+++ src/app/components/layout/InitClient.tsx
@@ -12,6 +12,8 @@ import { SWRConfig } from 'swr';
 import { ziggleApi } from '@/api';
 import { PropsWithLng } from '@/app/i18next';
 
+import ThemeToaster from './ThemeToaster';
+
 const InstallApp = dynamic(() => import('./InstallApp'), {
   ssr: false,
 });
@@ -24,23 +26,28 @@ const InitClient = ({
   lng,
   children,
   emptyLayout = false,
-}: React.PropsWithChildren<PropsWithLng & InitClientProps>) => (
-  <SWRConfig
-    value={{ fetcher: (path) => ziggleApi.get(path).then(({ data }) => data) }}
-  >
-    <SessionProvider>
-      <OverlayProvider>
-        {!emptyLayout && (
-          <Suspense>
-            <InstallApp lng={lng} />
-          </Suspense>
-        )}
-        <ThemeProvider enableSystem defaultTheme="system">
-          {children}
-        </ThemeProvider>
-      </OverlayProvider>
-    </SessionProvider>
-  </SWRConfig>
-);
+}: React.PropsWithChildren<PropsWithLng & InitClientProps>) => {
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (path) => ziggleApi.get(path).then(({ data }) => data),
+      }}
+    >
+      <SessionProvider>
+        <OverlayProvider>
+          {!emptyLayout && (
+            <Suspense>
+              <InstallApp lng={lng} />
+            </Suspense>
+          )}
+          <ThemeProvider enableSystem defaultTheme="system">
+            <ThemeToaster />
+            {children}
+          </ThemeProvider>
+        </OverlayProvider>
+      </SessionProvider>
+    </SWRConfig>
+  );
+};
 
 export default InitClient;
--- src/app/components/shared/Zabo/ZaboActions.tsx
+++ src/app/components/shared/Zabo/ZaboActions.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { toast } from 'react-hot-toast';
+import { toast } from 'sonner';
 import Swal from 'sweetalert2';
 
 import LogEvents from '@/api/log/log-events';
