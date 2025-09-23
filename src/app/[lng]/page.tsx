dc47efb515ed053e24afd5433255e0e56af8c457
SHIM-JINSEO <sjs011828@gmail.com>
2025-09-24 03:19:27 +0900

    feat: show modal for check personal info process

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 
 import { ziggleApi } from '@/api';
 import { PropsWithLng } from '@/app/i18next';
@@ -39,6 +40,35 @@ export default function Home({ params: { lng } }: { params: PropsWithLng }) {
       }
     })();
   }, [session, status]);
+
+  useEffect(() => {
+    if (openModal) {
+      Swal.fire({
+        title: '개인정보 약관 동의',
+        text: '(개인정보 약관 보여주기)',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: '동의',
+        cancelButtonText: '취소',
+        //Show Terms and Conditions/Personal Information Processing
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: '동의 완료',
+            text: '약관에 동의하셨습니다.',
+            icon: 'success',
+          });
+          setOpenModal(false);
+          // route.psuh(`${lng}/home`);
+          // TODO: Post consent of user as true
+        } else {
+          setOpenModal(false);
+          // Check user's intentaion again
+          // TODO: Withdrwal
+        }
+      });
+    }
+  }, [openModal]);
 
   return (
     <ThemeProvider>
