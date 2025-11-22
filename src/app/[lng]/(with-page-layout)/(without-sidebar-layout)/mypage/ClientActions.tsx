0011ceae962b0d8847bcd1d030056a90c71ad140
Suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2025-11-22 18:51:27 +0900

    feat: add withdrawal modal messages with i18n

--- src/app/[lng]/(with-page-layout)/(without-sidebar-layout)/mypage/ClientActions.tsx
+++ src/app/[lng]/(with-page-layout)/(without-sidebar-layout)/mypage/ClientActions.tsx
@@ -21,29 +21,38 @@ export default function ClientActions({ lng }: PropsWithLng) {
   };
 
   const handleWithdrawal = () => {
+    const deleteUser = async () => {
+    await ziggleApi.delete('/user');
+    };
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
-        confirmButton: "btn btn-success",
-        cancelButton: "btn btn-danger"
+        confirmButton: "text-gray px-4 py-2 rounded hover:bg-red-400 hover:text-white",
+        cancelButton: "text-gray px-4 py-2 rounded hover:bg-red-400 hover:text-white",
+
+        //confirmButton: "btn btn-success",
+        //cancelButton: "btn btn-danger"
       },
       buttonsStyling: false
     });
     swalWithBootstrapButtons.fire({
-      title: "정말 탈퇴하시겠습니까?",
-      text: "탈퇴 버튼 선택 시, 계정은 삭제되며 복구되지 않습니다.",
+      title: t('mypage.withdrawal.confirm.title'),
+      text: t('mypage.withdrawal.confirm.text'),
+      //title: "정말 탈퇴하시겠습니까?",
+      //text: "탈퇴 버튼 선택 시, 계정은 삭제되며 복구되지 않습니다.",
       icon: "warning",
       showCancelButton: true,
-      confirmButtonText: "탈퇴",
-      cancelButtonText: "취소",
+      confirmButtonText: t('mypage.withdrawal.confirm.okBtn'),
+      cancelButtonText: t('mypage.withdrawal.confirm.cancelBtn'),
       reverseButtons: true
     }).then((result) => {
       if (result.isConfirmed) {
-        ziggleApi.delete(`/user`); 
+        deleteUser();
         //index.ts에 설명 적어둠. fetch대신 ziggleApi.delete쓰기. 
         //원래 경로는 https://api.stg.ziggle.gistory.me/user인데 bff써서 /user만 쓰는거.
         swalWithBootstrapButtons.fire({
-          title: "탈퇴 완료",
-          text: "회원님의 Ziggle 계정이 성공적으로 삭제되었습니다.",
+          title: t('mypage.withdrawal.success.title'),
+          text: t('mypage.withdrawal.success.text'),
+          //text: "회원님의 Ziggle 계정이 성공적으로 삭제되었습니다.",
           icon: "success"
         });
       } else if (
