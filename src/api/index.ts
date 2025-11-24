12368f97318fd5d33d7b541f465a81e066d75612
Suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2025-11-24 20:56:47 +0900

    fix: remove comments

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,8 @@ import axios from 'axios';
 import { auth } from './auth/auth';
 
 const ziggleApi = axios.create({ baseURL: '/api/bff' }); 
-//axios는 토큰 만들어둔거 갖다 쓰는거. fetch쓰면 일일이 method, headers등등 써줘야 하니까 axios로 만들어둔거 쓰기.
-//src/api/bff에서 경로 설정해둠.
-ziggleApi.interceptors.request.use(async (config) => { //axios interceptors는 response가로채서 토큰 넣기. 
+
+ziggleApi.interceptors.request.use(async (config) => {
   if (typeof window !== 'undefined') return config;
 
   config.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL!;
