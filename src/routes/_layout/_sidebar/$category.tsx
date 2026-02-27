ac357ef4c10316fc2e380a18386cf9ff46c6309f
2paperstar <lee@paperst.ar>
2026-02-28 04:46:24 +0900

    fix: lowercase category params stringify

--- src/routes/_layout/_sidebar/$category.tsx
+++ src/routes/_layout/_sidebar/$category.tsx
@@ -7,14 +7,19 @@ import { Category } from '@/features/notice/models';
 
 export const Route = createFileRoute('/_layout/_sidebar/$category')({
   component: NoticeListFrame,
-  params: z.object({
-    category: z
-      .string()
-      .toUpperCase()
-      .pipe(z.enum(Category))
-      .or(z.enum(['home', 'deadline', 'zigglepick', 'reminded', 'own']))
-      .catch('home'),
-  }),
+  params: {
+    parse: z.object({
+      category: z
+        .string()
+        .toUpperCase()
+        .pipe(z.enum(Category))
+        .or(z.enum(['home', 'deadline', 'zigglepick', 'reminded', 'own']))
+        .catch('home'),
+    }).parse,
+    stringify: (params) => ({
+      category: params.category.toLowerCase(),
+    }),
+  },
   validateSearch: z.object({
     deadline: z.boolean().optional().default(false),
     page: z.number().optional().default(0),
