1a7ad9e79fe88123608d45fcee2f154254733417
2paperstar <lee@paperst.ar>
2026-02-27 19:22:40 +0900

    chore: init dayjs

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 import { TanStackDevtools } from '@tanstack/react-devtools';
+import '@/common/lib/dayjs-init';
 
 import '../styles.css';
 
