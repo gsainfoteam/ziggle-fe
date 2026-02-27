a0079b93f019157e71b9d9182264752b71d8d149
2paperstar <lee@paperst.ar>
2026-02-27 18:10:04 +0900

    chore: add tailwind prettier

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,8 @@
 const config = {
   semi: false,
   singleQuote: true,
-  trailingComma: "all",
-};
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
 
-export default config;
+export default config
