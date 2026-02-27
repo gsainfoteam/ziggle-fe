7eb4c92126b3e81eebadc17c06c8b328b33b0648
2paperstar <lee@paperst.ar>
2026-02-27 17:45:02 +0900

    chore: empty project

--- src/api/notice/send-alarm.ts
+++
@@ -1,5 +0,0 @@
-import { ziggleApi } from '..';
-import type { Notice } from './notice';
-
-export const sendNoticeAlarm = ({ id }: Pick<Notice, 'id'>) =>
-  ziggleApi.post(`/notice/${id}/alarm`).then((res) => res.data);
--- src/app/[lng]/(with-page-layout)/(without-sidebar-layout)/mypage/MypageBox.tsx
+++
@@ -1,9 +0,0 @@
-const MypageBox = ({ children }: React.PropsWithChildren) => {
-  return (
-    <div className="flex flex-col items-center gap-4 rounded-xl bg-greyLight p-4 dark:bg-dark_greyDark">
-      {children}
-    </div>
-  );
-};
-
-export default MypageBox;
