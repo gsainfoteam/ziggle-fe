2afb92797c333286bf6c8b81372ba83451a0a5ba
SHIM-JINSEO <sjs011828@gmail.com>
2026-03-07 18:55:21 +0900

    feat: add new tailwindcss utility for animation and create FloatingBlock components
    TODO: animation of blocks are not my expectations now. It must be changed

--- src/features/landing/LandingModal/LandingModal.tsx
+++ src/features/landing/LandingModal/LandingModal.tsx
@@ -4,6 +4,8 @@ import ZiggleLogoDark from '@/assets/logos/ziggle-dark.svg?react';
 import ZiggleLogo from '@/assets/logos/ziggle.svg?react';
 import { useAuth, useAuthRedirect } from '@/features/auth';
 import { useSearch } from '@tanstack/react-router';
+
+import FloatingBlock from './FloatingBlock';
 export default function LandingModal() {
   const { t } = useTranslation('home');
   const { redirect } = useSearch({ from: '/_auth' });
@@ -11,7 +13,7 @@ export default function LandingModal() {
 
   return (
     <div className="flex h-145 w-233.5 rounded-2xl px-2.5">
-      <div></div>
+      <FloatingBlock /> 
       <div className="flex w-96 flex-col justify-between px-4 py-2">
         <div className="flex h-110 flex-col items-center justify-center">
           <div className="mb-5 block dark:hidden">
