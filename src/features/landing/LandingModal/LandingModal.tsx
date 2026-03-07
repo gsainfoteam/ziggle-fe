183b4e973c7225106ee3c73024d729e1ab9bc152
SHIM-JINSEO <sjs011828@gmail.com>
2026-03-07 20:53:12 +0900

    feat: add backdrop and change useSeach

--- src/features/landing/LandingModal/LandingModal.tsx
+++ src/features/landing/LandingModal/LandingModal.tsx
@@ -8,35 +8,40 @@ import { useSearch } from '@tanstack/react-router';
 import IrregularGrids from './IrregularGrids';
 export default function LandingModal() {
   const { t } = useTranslation('home');
-  const { redirect } = useSearch({ from: '/_auth' });
+
+  const search = useSearch({ strict: false });
+  const redirect  = search.redirect
   const { idpLogIn } = useAuth();
 
   return (
-    <div className="flex h-145 w-233.5 rounded-2xl px-2.5">
-      <IrregularGrids />
-      <div className="flex w-96 flex-col justify-between px-4 py-2">
-        <div className="flex h-110 flex-col items-center justify-center">
-          <div className="mb-5 block dark:hidden">
-            <ZiggleLogo className="w-55" />
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+      <div className="flex h-145 w-233.5 rounded-2xl bg-white px-2.5 shadow-xl">
+        <IrregularGrids />
+        <div className="flex w-96 flex-col justify-between p-6">
+          <div className="flex h-110 flex-col items-center justify-center">
+            <div className="mb-5 block dark:hidden">
+              <ZiggleLogo className="w-55" />
+            </div>
+            <div className="mb-5 hidden dark:block">
+              <ZiggleLogoDark className="w-55" />
+            </div>
+            <div className="text-lg font-bold">{t('home.subtitle')}</div>
           </div>
-          <div className="mb-5 hidden dark:block">
-            <ZiggleLogoDark className="w-55" />
+          <div className="flex h-fit flex-col items-center gap-3">
+            <p className="font-xs text-greyDark font-medium">
+              {t('home.policy')}
+            </p>
+            <Button
+              className="w-80"
+              variant="outlined"
+              onClick={() => {
+                useAuthRedirect.getState().setRedirect(redirect ?? '/home');
+                idpLogIn();
+              }}
+            >
+              {t('home.login')}
+            </Button>
           </div>
-          <div className="text-lg font-bold">{t('home.subtitle')}</div>
-        </div>
-        <div className="flex h-fit flex-col items-center gap-3">
-          <p className="font-xs text-greyDark font-medium">
-            {t('home.policy')}
-          </p>
-          <Button
-            variant="outlined"
-            onClick={() => {
-              useAuthRedirect.getState().setRedirect(redirect ?? '/home');
-              idpLogIn();
-            }}
-          >
-            {t('home.login')}
-          </Button>
         </div>
       </div>
     </div>
